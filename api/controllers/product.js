e95942afb2021867d9b503fb82405b00cc02faca
dhany <dhanyaritonang@gmail.com>
2020-01-31 06:45:12 +0700

    get all products

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,6 +1,19 @@
 const model = require('../db/models');
 
 module.exports = {
-  getProducts: async (req, res) => null,
+  getProducts: async (req, res) => {
+    try {
+      const { count, rows } = await model.products.findAndCountAll();
+      res.json({
+        status: 'Success',
+        code: 200,
+        message: 'Success get all data products',
+        count,
+        data: rows,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
   getOneProduct: async (req, res) => null,
 };
